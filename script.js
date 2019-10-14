1a46a5caee1f7c67c10936ebb239f21edca38b6d
Shobhit Agarwal <shobhit.agarwal@simplifai.ai>
2019-10-14 17:17:01 +0530

    js upgrades

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+  let vidCount = 0;
+
   $(document).ready(function(){
+  	setDefaultVids();
   	$("#load-more-btn").click(function(){
   		(function addVidEle(num) {
-  			$("#vid-list").append(vidEle); 
+  			if( vidCount < 10 ) {
+  				vidCount++;
+  				$("#vid-list").append(addVidEle(id, title, desc)); 
+  			}
   			if (num > 1) addVidEle(num - 1);
   		})(3);
   	});
   });
 
-  let vidEle = 
+  const addVidEle = (id, title, desc) =>
   `<div class="container">
   <div class="row video-slider-row">
   <div class="col-sm-12 col-md-4 col-lg-4 video-info-left">
@@ -16,11 +22,9 @@
   </div>
   <div class="col-sm-12 col-md-8 col-lg-8 video-info-right">
   <div class="">
-  <div class="video-content">
-  <h1>Introducing the Canon XF405 & XF400 Camcorders</h1>
-  <p>Really compact light form factor and all the features and quality that I want versus a more
-  expensive and heavier higher end camera. Wi-Fi connectivity to iPhone and Laptop is VERY
-  convenient. I use the iPhone as a remote trigger for low light.</p>
+  <div id="${id}" class="video-content">
+  <h1>${title}</h1>
+  <p>${desc}</p>
   </div>
   </div>
   </div>
